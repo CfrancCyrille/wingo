ba5172db15e81d059ff301f121f229b6b96d9ca5
Cyrille FRANCOIS <francois.cyrille@gmail.com>
2015-12-06 18:38:51 +0100

    Init

---
+++ wingo/src/test/java/cfranc/com/wingo/UserTest.java
@@ -0,0 +1,19 @@
+package cfranc.com.wingo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testUser() {
+		assertTrue("Not yet implemented",true);
+	}
+
+	@Test
+	public void testGetLogin() {
+		assertTrue("Not yet implemented",true);
+	}
+
+}
